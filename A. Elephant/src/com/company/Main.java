6a42865ba25951964c64d03114f3ee10c1d30632
irazramin <irazramin@gmail.com>
2021-12-01 19:47:43 +0600

    A. Word

--- A. Elephant/src/com/company/Main.java
+++ A. Elephant/src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -9,20 +9,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
-        ArrayList<Float> res = new ArrayList<>();
-        int[] moves = new int[]{1, 2, 3, 4, 5};
-        double res1 = 0.0;
-        for (int i = 0; i<moves.length; i++){
-           res1 = x%moves[i];
-            print(res1);
+        int[] d = new int[]{1, 2, 3, 4, 5};
+        ArrayList<Double> res = new ArrayList<>();
+
+        for (int i = 0; i < d.length; i++) {
+            res.add((double) x / d[i]);
         }
+        Collections.sort(res);
 
-
-
+        for (Double index : res){
+            print(Math.ceil(index));
+            break;
+        }
 
     }
 
-    public static void print(double output){
-        System.out.println(output);
+    public static void print(double output) {
+        System.out.println((int) output);
     }
 }
